c75883c7b55db73cf8c570af649a70d80923dd35
Mingyu Liu <liumy@seagroup.com>
2018-07-14 23:17:55 +0800

    Added ESLint

---
+++ www/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            experimentalObjectRestSpread: true,
+            "jsx": true
+        },
+        "ecmaVersion": 7,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- www/src/PageHome/index.js
+++ www/src/PageHome/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import * as styles from './style.scss';
+import React from "react";
+import * as styles from "./style.scss";
 
 class PageHome extends React.Component {
-  render() {
-    return (
-      <div className={styles.home_page}>
-        <h1>Hello, world!</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className={styles.home_page}>
+                <h1>Hello, world!</h1>
+            </div>
+        );
+    }
 }
 
 export default PageHome;
