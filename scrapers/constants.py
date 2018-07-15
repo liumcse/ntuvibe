91fdd91948436e3e1b8542cfadd14d00f4b4def6
chenchongsong <chenchongsong@outlook.com>
2018-07-15 17:12:19 +0800

    scraper: class_schedule

--- scrapers/constants.py
+++ scrapers/constants.py
@@ -1,5 +1,5 @@
 COURSE_CONTENT_MAIN_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/aus_subj_cont.main"
-COURSE_CONTENT_SEMESTER_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SUBJ_CONT.main_display"
+COURSE_CONTENT_CATEGORY_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SUBJ_CONT.main_display"
 COURSE_CONTENT_DETAIL_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SUBJ_CONT.main_display1"
 
 
@@ -16,3 +16,9 @@ class COURSE_CONTENT_DETAIL_TYPE:
 
     NAME_TO_VALUE = dict((k, v) for k, v in locals().items())
     VALUE_TO_NAME = dict((v, k) for k, v in NAME_TO_VALUE.items())
+
+
+CLASS_SCHEDULE_MAIN_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/aus_schedule.main"
+CLASS_SCHEDULE_CATEGORY_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display"
+CLASS_SCHEDULE_DETAIL_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display1"
+
--- scrapers/scraper_course_content/crawl.py
+++ scrapers/scraper_course_content/crawl.py
@@ -1,3 +1,4 @@
+import os
 import json
 import time
 from bs4 import BeautifulSoup
@@ -35,9 +36,9 @@ def parse_course_details(detail_html):
 
     global all_course_details
     for table in soup.find_all("table"):  # each table contains info of a course
+        course_detail = dict()
 
         # extract prerequisite info (in pink color)
-        course_detail = dict()
         course_detail["prerequisite"] = []
         pink_fonts = table.find_all("font", attrs={"color": "#FF00FF"})
         for font in pink_fonts:
@@ -45,6 +46,7 @@ def parse_course_details(detail_html):
                 course_detail["prerequisite"].append(font.text)
             font.extract()
 
+        # put remaining text into a deque
         texts = deque([td.text for td in table.find_all("td")])
         course_code = texts.popleft()
         if all_course_details.get(course_code):
@@ -68,21 +70,25 @@ def parse_course_details(detail_html):
 
 
 def crawl():
-    main_site_html = request_manager.retrieve_main_html()
+    main_site_html = request_manager.get_course_content_main_html()
     latest_semester = parse_latest_semester(main_site_html)
 
-    semester_list_html = request_manager.retrieve_semester_list_html(latest_semester)
-    category_list = parse_category_list(semester_list_html)
+    category_list_html = request_manager.get_course_content_category_list_html(latest_semester)
+    category_list = parse_category_list(category_list_html)
 
     for category in category_list:
         print(category)
-        detail_html = request_manager.retrieve_detail_html(latest_semester, category)
+        detail_html = request_manager.get_course_content_detail_html(latest_semester, category)
         parse_course_details(detail_html)
 
         time.sleep(0.1)
 
     global all_course_details
-    write_json = open("data/course_content_%s.json" % get_timestamp(), "wb")
+
+    write_json_path = "data/course_content_%s.json"
+    if not os.path.exists(os.path.dirname(write_json_path)):
+        os.makedirs(os.path.dirname(write_json_path))
+    write_json = open(write_json_path % get_timestamp(), "wb")
     write_json.write(json.dumps(all_course_details, ensure_ascii=False, indent=2).encode('utf8'))
     return all_course_details
 
