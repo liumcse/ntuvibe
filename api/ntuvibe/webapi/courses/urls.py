fb4525ff45e93d5d4b3bbbc38b70da69e3db3dbb
Chen Chongsong <chencs@seagroup.com>
2018-07-31 00:37:19 +0800

    support cors

--- api/ntuvibe/webapi/courses/urls.py
+++ api/ntuvibe/webapi/courses/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path(r'get_course_list/', views.get_course_list),
-	path(r'get_course_detail/', views.get_course_detail),
-	path(r'get_class_schedule/', views.get_class_schedule),
-	path(r'get_exam_schedule/', views.get_exam_schedule),
-	path(r'add_course_rating/', views.add_course_rating),
+	path(r'get_course_list', views.get_course_list),
+	path(r'get_course_detail', views.get_course_detail),
+	path(r'get_class_schedule', views.get_class_schedule),
+	path(r'get_exam_schedule', views.get_exam_schedule),
+	path(r'add_course_rating', views.add_course_rating),
 ]
--- api/ntuvibe/webapi/profs/urls.py
+++ api/ntuvibe/webapi/profs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path(r'add_prof/<int:course_id>/$', views.create_prof),
+	path(r'add_prof/<int:course_id>', views.create_prof),
 	path(r'add_prof_rating/<int:prof_id>', views.add_prof_rating),
 ]
