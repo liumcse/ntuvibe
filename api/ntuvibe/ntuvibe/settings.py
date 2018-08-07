2e42653aa7f782eb8105e2c19a7dc8b554bca283
Chen Chongsong <chencs@seagroup.com>
2018-08-07 17:54:41 +0800

    deploy and time_zone of logging

--- api/ntuvibe/ntuvibe/settings.py
+++ api/ntuvibe/ntuvibe/settings.py
@@ -131,14 +131,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'en'
+TIME_ZONE = 'Asia/Singapore'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
