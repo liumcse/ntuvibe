0606a18aca79211cb4cdc586e614aca4b195662b
chenchongsong <chenchongsong@outlook.com>
2018-08-19 17:44:00 +0800

    update cronjob

--- scrapers/cronjobs/course_content_to_db.py
+++ scrapers/cronjobs/course_content_to_db.py
@@ -3,6 +3,7 @@ import django
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../api/ntuvibe")
+sys.path.append("/home/ubuntu/.local/lib/python3.5/site-packages")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ntuvibe.settings")
 django.setup()
 
