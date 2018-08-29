ebee4bb4efa6b56ad12c739e5197087f67ca4f9d
chenchongsong <chenchongsong@outlook.com>
2018-08-29 13:42:01 +0800

    modify scraper code styles

--- scrapers/constants.py
+++ scrapers/constants.py
@@ -1,27 +1,7 @@
 COURSE_CONTENT_MAIN_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/aus_subj_cont.main"
 COURSE_CONTENT_DETAIL_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SUBJ_CONT.main_display1"
-CHECK_VACANCIES_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/aus_vacancy.check_vacancy2"
-
-
-class CourseContentDetailType:
-    prerequisite = "Prerequisite:"
-    grade_type = "Grade Type: "
-    mutex = "Mutually exclusive with: "
-    na_to = "Not available to Programme: "
-    na_to_all = "Not available to all Programme with: "
-    na_to_as_core = "Not available as Core to Programme: "
-    na_to_as_pe = "Not available as PE to Programme: "
-    na_to_as_ue = "Not available as UE to Programme: "
-    na_to_as_ue_nationality = "Not available as UE to Nationality: "
-    na_to_as_ue_race = "Not available as UE to Race: "
-    remark = "Remark:"
-
-    NAME_TO_VALUE = dict((k, v) for k, v in locals().items())
-    VALUE_TO_NAME = dict((v, k) for k, v in NAME_TO_VALUE.items())
-
 
 CLASS_SCHEDULE_MAIN_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/aus_schedule.main"
-CLASS_SCHEDULE_CATEGORY_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display"
 CLASS_SCHEDULE_DETAIL_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/AUS_SCHEDULE.main_display1"
 
 EXAM_SCHEDULE_MAIN_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/exam_timetable_und.MainSubmit"
@@ -29,37 +9,56 @@ EXAM_SCHEDULE_DETAIL_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/exam_timetabl
 
 EXAM_SCHEDULE_USEFUL_SEMESTER_VALUE = "3"
 
+COURSE_VACANCIES_URL = "https://wish.wis.ntu.edu.sg/webexe/owa/aus_vacancy.check_vacancy2"
+
+
+class CourseContentDetailType:
+	prerequisite = "Prerequisite:"
+	grade_type = "Grade Type: "
+	mutex = "Mutually exclusive with: "
+	na_to = "Not available to Programme: "
+	na_to_all = "Not available to all Programme with: "
+	na_to_as_core = "Not available as Core to Programme: "
+	na_to_as_pe = "Not available as PE to Programme: "
+	na_to_as_ue = "Not available as UE to Programme: "
+	na_to_as_ue_nationality = "Not available as UE to Nationality: "
+	na_to_as_ue_race = "Not available as UE to Race: "
+	remark = "Remark:"
+
+	NAME_TO_VALUE = dict((k, v) for k, v in locals().items())
+	VALUE_TO_NAME = dict((v, k) for k, v in NAME_TO_VALUE.items())
+
 
 #  For analysis of crawled data
 class Days:
-    MONDAY = 1
-    TUESDAY = 2
-    WEDNESDAY = 3
-    THURSDAY = 4
-    FRIDAY = 5
-    SATURDAY = 6
-    SUNDAY = 7
+	MONDAY = 1
+	TUESDAY = 2
+	WEDNESDAY = 3
+	THURSDAY = 4
+	FRIDAY = 5
+	SATURDAY = 6
+	SUNDAY = 7
 
-    NAME_TO_VALUE = dict((k, v) for k, v in locals().items())
-    VALUE_TO_NAME = dict((v, k) for k, v in NAME_TO_VALUE.items())
-    VALUE_TO_NAME.update({"MON": 1, "TUE": 2, "WED": 3, "THU": 4, "FRI": 5, "SAT": 6, "SUN": 7})
+	NAME_TO_VALUE = dict((k, v) for k, v in locals().items())
+	VALUE_TO_NAME = dict((v, k) for k, v in NAME_TO_VALUE.items())
+	VALUE_TO_NAME.update({"MON": 1, "TUE": 2, "WED": 3, "THU": 4, "FRI": 5, "SAT": 6, "SUN": 7})
 
 
 class GradeType:
-    default = 0
-    pass_or_fail = 1
+	default = 0
+	pass_or_fail = 1
 
-    ID_TO_READABLE = {
-        0: "Default",
-        1: "Pass/Fail",
-    }
-    READABLE_TO_ID = dict((v, k) for k, v in ID_TO_READABLE.items())
+	ID_TO_READABLE = {
+		0: "Default",
+		1: "Pass/Fail",
+	}
+	READABLE_TO_ID = dict((v, k) for k, v in ID_TO_READABLE.items())
 
 
 CONSTRING_KEYS = []
 for key in CourseContentDetailType.NAME_TO_VALUE.keys():
-    if not key.startswith("__"):
-        CONSTRING_KEYS.append(key)
+	if not key.startswith("__"):
+		CONSTRING_KEYS.append(key)
 CONSTRING_KEYS.remove("grade_type")
 CONSTRING_KEYS.remove("remark")
 
--- scrapers/request_manager.py
+++ scrapers/request_manager.py
@@ -3,78 +3,65 @@ from scrapers.constants import *
 
 
 def get(url, *args, **kwargs):
-    return requests.get(url, params=kwargs).text.encode('utf-8')
+	return requests.get(url, params=kwargs).text.encode('utf-8')
 
 
 def post(url, headers, data, verify=False):
-    return requests.post(url, data=data, headers=headers, verify=verify).text.encode('utf-8')
+	return requests.post(url, data=data, headers=headers, verify=verify).text.encode('utf-8')
 
 
 def get_course_content_main_html():
-    return get(COURSE_CONTENT_MAIN_URL)
+	return get(COURSE_CONTENT_MAIN_URL)
 
 
 def get_course_content_detail_html(semester):
-    return post(COURSE_CONTENT_DETAIL_URL,
-                {},
-                {"acadsem":semester,
-                  "r_course_yr":"",
-                  "r_subj_code":"",
-                  "boption":"Search",
-                  "acad":semester[0:4],
-                  "semester":semester[-1]})
+	return get(
+		COURSE_CONTENT_DETAIL_URL,
+		acadsem=semester,
+		r_course_yr="",
+		r_subj_code="",
+		boption="Search",
+		acad=semester[0:4],
+		semester=semester[-1]
+	)
 
 
 def get_class_schedule_main_html():
-    return get(CLASS_SCHEDULE_MAIN_URL)
+	return get(CLASS_SCHEDULE_MAIN_URL)
 
 
-def get_class_schedule_category_list_html(semester):
-    return get(CLASS_SCHEDULE_CATEGORY_URL,
-               acadsem=semester,
-               r_subj_code="Enter Keywords or Course Code",
-               r_search_type="F",
-               boption="x",
-               staff_access="false")
-
-
-def get_class_schedule_detail_html(semester, category):
-    return get(CLASS_SCHEDULE_DETAIL_URL,
-               acadsem=semester,
-               r_course_yr=category,
-               r_subj_code="Enter Keywords or Course Code",
-               boption="CLoad",
-               staff_access="false")
-
-def get_all_class_schedule_detail_html(semester):
-    return get(CLASS_SCHEDULE_DETAIL_URL,
-               acadsem=semester,
-               r_course_yr="",
-               r_search_type="F",
-               r_subj_code="",
-               boption="Search",
-               staff_access="false")
-
+def get_class_schedule_detail_html(semester):
+	return get(
+		CLASS_SCHEDULE_DETAIL_URL,
+		acadsem=semester,
+		r_course_yr="",
+		r_search_type="F",
+		r_subj_code="",
+		boption="Search",
+		staff_access="false"
+	)
 
 
 def get_exam_schedule_main_html():
-    return get(EXAM_SCHEDULE_MAIN_URL, p_opt=1, bOption="Next")
+	return get(EXAM_SCHEDULE_MAIN_URL, p_opt=1, bOption="Next")
 
 
 def get_exam_schedule_detail_html(academic_year, semester):
-    return get(EXAM_SCHEDULE_DETAIL_URL,
-               p_exam_dt="",
-               p_start_time="",
-               p_dept="",
-               p_subj="",
-               p_venue="",
-               p_matric="",
-               academic_session="Semester %s Academic Year %s-%s" % (semester, int(academic_year), int(academic_year)+1),
-               p_plan_no=EXAM_SCHEDULE_USEFUL_SEMESTER_VALUE,
-               p_exam_yr=academic_year,
-               p_semester=semester,
-               bOption="Next")
+	return get(
+		EXAM_SCHEDULE_DETAIL_URL,
+		p_exam_dt="",
+		p_start_time="",
+		p_dept="",
+		p_subj="",
+		p_venue="",
+		p_matric="",
+		academic_session="Semester %s Academic Year %s-%s" % (semester, int(academic_year), int(academic_year)+1),
+		p_plan_no=EXAM_SCHEDULE_USEFUL_SEMESTER_VALUE,
+		p_exam_yr=academic_year,
+		p_semester=semester,
+		bOption="Next"
+	)
 
 
 def get_course_vacancy(subj):
-    return get(CHECK_VACANCIES_URL, subj=subj)
+	return get(COURSE_VACANCIES_URL, subj=subj)
