d137e1d1335ac294cf3e19ec19c97ad96d8b425e
Mingyu Liu <m325liu@uwaterloo.ca>
2018-09-14 17:16:41 +0800

    Delete redundant code

--- www/src/PageCourseDetail/components/Menu/index.js
+++ www/src/PageCourseDetail/components/Menu/index.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { popupTrigger } from "src/redux/actions";
 import RateCourse from "src/components/Popups/_RateCourse";
 import Button from "antd/lib/button";
 
@@ -12,7 +9,6 @@ import * as theme from "./theme.scss";
 
 class Menu extends React.PureComponent {
   render() {
-    const { popupTrigger } = this.props;
     return (
       <div className={styles.menu}>
         <div className={styles.search}>
@@ -20,7 +16,6 @@ class Menu extends React.PureComponent {
         </div>
         <div className={styles.actions}>
           <div className={styles.action}>
-            {/* <button className={styles.rateCourse}>Rate the course</button> */}
             <RateCourse
               trigger={<Button type="primary">Rate the course</Button>}
             />
@@ -31,15 +26,4 @@ class Menu extends React.PureComponent {
   }
 }
 
-Menu.propTypes = {
-  popupTrigger: PropTypes.func.isRequired
-};
-
-const mapDispatchToProps = dispatch => ({
-  popupTrigger: option => dispatch(popupTrigger(option))
-});
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Menu);
+export default Menu;
