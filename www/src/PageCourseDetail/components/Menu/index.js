38a4d2264bc1ab4bc14512db7cfec138435312dd
Mingyu Liu <m325liu@uwaterloo.ca>
2018-09-15 16:12:30 +0800

    Added rate button at smaller screen

--- www/src/PageCourseDetail/components/Menu/index.js
+++ www/src/PageCourseDetail/components/Menu/index.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { popupTrigger } from "src/redux/actions";
 import Button from "antd/lib/button";
+import Icon from "antd/lib/icon";
 
 import Search from "src/components/Search";
 
@@ -13,17 +14,28 @@ class Menu extends React.PureComponent {
   render() {
     const { popupTrigger } = this.props;
     return (
-      <div className={styles.menu}>
-        <div className={styles.search}>
-          <Search theme={theme} />
-        </div>
-        <div className={styles.actions}>
-          <div className={styles.action} onClick={() => popupTrigger(3)}>
-            {/* <button className={styles.rateCourse}>Rate the course</button> */}
-            <Button type="primary">Rate the course</Button>
+      <React.Fragment>
+        <div className={styles.menu}>
+          <div className={styles.search}>
+            <Search theme={theme} />
+          </div>
+          <div className={styles.actions}>
+            <div className={styles.action} onClick={() => popupTrigger(3)}>
+              {/* <button className={styles.rateCourse}>Rate the course</button> */}
+              <Button type="primary">Rate the course</Button>
+            </div>
           </div>
         </div>
-      </div>
+        <Button
+          className={styles.roundButton}
+          onClick={() => popupTrigger(3)}
+          style={{ position: "fixed", display: "none" }}
+          type="primary"
+          shape="circle"
+          size="large"
+          icon="edit"
+        />
+      </React.Fragment>
     );
   }
 }
