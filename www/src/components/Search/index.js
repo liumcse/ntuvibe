3cb1387fc79d8d79bc47f466fe46f9a93ae8b141
Zayn Jarvis <ZaynJarvis@gmail.com>
2019-01-19 11:31:19 +0800

    fix trailing whitespace

--- www/src/components/Search/index.js
+++ www/src/components/Search/index.js
@@ -168,7 +168,7 @@ class Search extends React.Component<Props, States> {
     const inputProps = {
       autoFocus: true,
       placeholder:
-        " Type the code or title of a course (e.g. CZ3003 or Algorithms)",
+        "Type the code or title of a course (e.g. CZ3003 or Algorithms)",
       value,
       disabled: !loaded,
       onChange: this.onChange
