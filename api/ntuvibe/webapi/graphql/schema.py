81c9107918a3aaa6bde2f4785570ea904572c039
Ming <liumcse@gmail.com>
2019-03-10 14:49:48 +0800

    Modify GraphQL schema

--- api/ntuvibe/webapi/graphql/schema.py
+++ api/ntuvibe/webapi/graphql/schema.py
@@ -9,11 +9,38 @@ class Courses(DjangoObjectType):
         model = CourseTab
 
 
+class CourseRatings(DjangoObjectType):
+
+    class Meta:
+        model = CourseRatingTab
+
+
+class ClassSchedules(DjangoObjectType):
+    class Meta:
+        model = ClassScheduleTab
+
+
 class Query(graphene.ObjectType):
     courses = graphene.List(Courses)
+    courseRatings = graphene.List(CourseRatings)
+    classSchedules = graphene.List(ClassSchedules)
 
     def resolve_courses(self, info):
         return CourseTab.objects.all()
+
+    def resolve_classSchedules(self, info):
+        return ClassScheduleTab.objects.all()
+
+    # def resolve_courseRatings(self, info):
+    #     return CourseRatingTab.objects.all()
+
+    def resolve_courseRatings(self, info, **kwargs):
+        if "courseCode" in kwargs:
+            courseCode = kwargs.get("courseCode")
+            course = CourseTab.objects.get(course_code=courseCode)
+            if course:
+                course_id = course.id
+                return CourseRatingTab.objects.get(id=course_id)
 
 
 schema = graphene.Schema(query=Query)
