36f68961bf79a817e297f04a71fb777a6ec5e06e
Ming <lyming90@gmail.com>
2019-03-12 22:54:53 +0800

    GraphQL (#102)
    * update to bable 7!
    
    * added async routing
    
    * preload and open search
    
    * added preload in webpack
    
    * preload
    
    * still try to preload
    
    * performance
    
    * Create AppShell for lazy loading
    
    * Fix style
    
    * Centralize load spinner
    
    * Fix align problem
    
    * Fix spinner position
    
    * Fix style
    
    * Remove preload
    
    * Change style but need more details
    
    * Change theme color
    
    * Update
    
    * Change graphics
    
    * Define a new way to override antd style
    
    * Amend style and alignment
    
    * Create static sites
    
    * Fix gulfile
    
    * Test form submission in Netlify
    
    * Submit for purely experimental purpose
    
    * Update libraries
    
    * Incorporate GraphQL :)
    
    * Fix things
    
    * Modify GraphQL schema
    
    * Upload workspace
    
    * Auto-generate sitemap before build
    
    * Fix package.json
    
    * Rewrite regex for search
    
    * Commit EOD

--- api/ntuvibe/webapi/manager/search_manager.py
+++ api/ntuvibe/webapi/manager/search_manager.py
@@ -2,46 +2,67 @@ from webapi.models import CourseTab, ExamScheduleTab
 from django.db.models import Q
 from .exam_schedule_manager import get_exam_schedule_by_course_id
 import urllib
+import itertools
+import re
 
 THIS_SEMESTER = "2018_2"
 
-def get_courses_by_search(keywords=None, filter=None, sort=0):
-	"""
-	Note on filters
-	0: Available in this semester
-	1: Pass / Fail
-	2: Read as PE
-	3: Read as UE
-	4: No final exam
-	"""
-	if keywords is None and filter is None:
-		return []
-	course = CourseTab.objects
-	keywords = urllib.parse.unquote(keywords).split(" ")
-	regex = r"(%s)+" % "+.*".join(keywords) # make regex
-	output = course.filter(Q(course_code__iregex=regex) | Q(course_title__iregex=regex))
-	# apply filters
-	if filter is not None:
-		filter = filter.split(" ")
-		for x in filter:
-			if x == "0":
-				output = output.filter(semesters__icontains=THIS_SEMESTER)
-			if x == "1":
-				output = output.filter(grade_type__exact="1")
-			if x == "2":
-				output = output.filter(as_pe__exact="1")
-			if x == "3":
-				output = output.filter(as_ue__exact="1")
-			if x == "4":
-				# todo
-				pass
 
-	return output
+def build_regex_from_keyword_list(keyword_list):
+    """This function builds regex pattern from a list of keywords
+
+    :param keyword_list: list of keywords
+    :type keyword_list: List
+    :return: regex
+    :rtype: str
+    """
+    splitted_keywords = urllib.parse.unquote(keyword_list).split("+")
+    permutated_keyword_list = list(
+        itertools.permutations(splitted_keywords))
+    regex_pre_build = list(
+        map(lambda x: "(.*" + "+.*".join(x) + ".*)+", permutated_keyword_list))
+    regex = re.compile("|".join(regex_pre_build)).pattern
+    return regex
+
+
+def get_courses_by_search(keywords=None, filter=None, sort=0):
+    """
+    Note on filters
+    0: Available in this semester
+    1: Pass / Fail
+    2: Read as PE
+    3: Read as UE
+    4: No final exam
+    """
+    if keywords is None and filter is None:
+        return []
+    regex = build_regex_from_keyword_list(keywords)
+    print(regex)
+    output = CourseTab.objects.filter(Q(course_code__iregex=regex)
+                                      | Q(course_title__iregex=regex) | Q(description__iregex=regex))
+    # apply filters
+    if filter is not None:
+        filter = filter.split(" ")
+        for x in filter:
+            if x == "0":
+                output = output.filter(semesters__icontains=THIS_SEMESTER)
+            if x == "1":
+                output = output.filter(grade_type__exact="1")
+            if x == "2":
+                output = output.filter(as_pe__exact="1")
+            if x == "3":
+                output = output.filter(as_ue__exact="1")
+            if x == "4":
+                # todo
+                pass
+
+    return output
+
 
 def prepare_search_result(courses):
-	return [{"code": course.course_code,
-					"title": course.course_title,
-					"description": course.description,
-					"as_ue": course.as_ue,
-					"as_pe": course.as_pe,
-					"grade_type": course.grade_type} for course in courses]
+    return [{"code": course.course_code,
+             "title": course.course_title,
+             "description": course.description,
+             "as_ue": course.as_ue,
+             "as_pe": course.as_pe,
+             "grade_type": course.grade_type} for course in courses]
---
+++ www/scripts/sitemap.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const path = require("path");
+
+const INCLUDED = `<url><loc>https://ntuvibe.com</loc><priority>0.5</priority></url>
+<url><loc>https://ntuvibe.com/about</loc><priority>0.5</priority></url>
+<url><loc>https://ntuvibe.com/scheduler</loc><priority>0.5</priority></url>
+<url><loc>https://ntuvibe.com/browser</loc><priority>0.5</priority></url>`;
+
+const PATH_OUTPUT = path.resolve(__dirname, "../src/brand/seo");
+
+console.log(PATH_OUTPUT, `${PATH_OUTPUT}/sitemap.xml`);
+
+axios
+  .get("https://api.ntuvibe.com/courses/get_course_list")
+  .then(response => {
+    const courseList = response.data.data; // weird huh
+    let urls = [];
+    courseList.forEach(element => {
+      const url = `<url><loc>https://ntuvibe.com/courses/${element.code.toLowerCase()}</loc><priority>0.5</priority></url>`;
+      urls.push(url);
+    });
+    const output = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${INCLUDED}
+${urls.join("\n")}
+</urlset>`;
+    fs.writeFile(`${PATH_OUTPUT}/sitemap.xml`, output, error => {
+      if (error) {
+        console.log("❗️  Error occurred during saving XML", error);
+      } else {
+        console.log("👏  Sitemap successfully generated!");
+      }
+    });
+  })
+  .catch(error => {
+    console.log("❗️  Error occurred!", error);
+  });
