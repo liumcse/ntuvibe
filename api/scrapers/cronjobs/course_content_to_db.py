d9d87cfbd82b5b0c1d46abd47d83d91605651059
Chongsong Chen <chongsong.chen@grabtaxi.com>
2019-03-20 13:03:32 +0800

    Fix scraper bug on as_ue & as_pe options

--- api/scrapers/cronjobs/course_content_to_db.py
+++ api/scrapers/cronjobs/course_content_to_db.py
@@ -27,8 +27,6 @@ def record_course_content(semester, course_code, course_content):
 		"description": course_content["description"],
 		"constraint": str(get_constraint_string(course_content)),
 		"grade_type": GradeType.READABLE_TO_ID[course_content.get("grade_type", "Default")],
-		"as_pe": False,
-		"as_ue": False,
 	}
 
 	course = CourseTab.objects.filter(course_code=course_code).first()
@@ -39,7 +37,11 @@ def record_course_content(semester, course_code, course_content):
 		kwargs.update({"semesters": str(semesters)})
 		CourseTab.objects.filter(course_code=course_code).update(**kwargs)
 	else:
-		kwargs.update({"semesters": str([semester])})
+		kwargs.update({
+			"semesters": str([semester]),
+			"as_pe": False,
+			"as_ue": False,
+		})
 		CourseTab.objects.create(course_code=course_code, **kwargs)
 
 
@@ -53,5 +55,8 @@ if __name__ == "__main__":
 		raise Exception("Invalid sys.argv length!")
 
 	course_contents = crawl_course_content.crawl(semester=semester)
+	print("Number of Courses Found: {}".format(len(course_contents.items())))
+
 	for course_code, course_content in course_contents.items():
 		record_course_content(semester, course_code, course_content)
+	print("Courses Dumped into DB")
