7091153e28411f5e5c66a1c58b1631b931e3ef75
Ming <liumcse@gmail.com>
2019-07-12 00:37:00 +0800

    Add endpoints

--- scraper/app.py
+++ scraper/app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 from crawl_course_content import crawl as crawl_course_content
 from crawl_course_content_graduate import crawl as crawl_course_content_graduate
 from crawl_class_schedule import crawl as crawl_class_schedule
 from crawl_exam_schedule import crawl as crawl_exam_schedule
 
-
 app = Flask(__name__)
 
-print(crawl_course_content())
+
+@app.route('/')
+def help():
+    return "Hi!"  # TODO(liumcse): Display help.
+
+
+@app.route('/course_content')
+def course_content():
+    return jsonify(crawl_course_content())
+
+
+@app.route('/course_content_graduate')
+def course_content_graduate():
+    return jsonify(crawl_course_content_graduate())
+
+
+@app.route('/class_schedule')
+def class_schedule():
+    return jsonify(crawl_class_schedule())
+
+
+@app.route('/exam_schedule')
+def exam_schedule():
+    return jsonify(crawl_exam_schedule())
