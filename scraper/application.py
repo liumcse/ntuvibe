e29295bb48300155a3de265aabab9f70129bf361
Ming <liumcse@gmail.com>
2019-07-12 22:25:05 +0800

    Rename

---
+++ scraper/application.py
@@ -0,0 +1,57 @@
+import traceback
+from flask import Flask, jsonify, make_response
+from crawl_course_content import crawl as crawl_course_content
+from crawl_course_content_graduate import crawl as crawl_course_content_graduate
+from crawl_class_schedule import crawl as crawl_class_schedule
+from crawl_exam_schedule import crawl as crawl_exam_schedule
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def help():
+    return "Hi!"  # TODO(liumcse): Display help.
+
+
+@application.route('/course_content')
+def course_content():
+    try:
+        data = crawl_course_content()
+        return jsonify(data)
+    except Exception as e:
+        response = make_response(traceback.format_exc(), 400)
+        return response
+
+
+@application.route('/course_content_graduate')
+def course_content_graduate():
+    try:
+        data = crawl_course_content_graduate()
+        return jsonify(data)
+    except Exception as e:
+        response = make_response(traceback.format_exc(), 400)
+        return response
+
+
+@application.route('/class_schedule')
+def class_schedule():
+    try:
+        data = crawl_class_schedule()
+        return jsonify(data)
+    except Exception as e:
+        response = make_response(traceback.format_exc(), 400)
+        return response
+
+
+@application.route('/exam_schedule')
+def exam_schedule():
+    try:
+        data = crawl_exam_schedule()
+        return jsonify(data)
+    except Exception as e:
+        response = make_response(traceback.format_exc(), 400)
+        return response
+
+
+if __name__ == "__main__":
+    application.run()
