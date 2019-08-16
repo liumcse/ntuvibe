b60df53dd1e0863d9c9caf79a3c50bd7424a398b
Ming <liumcse@gmail.com>
2019-08-17 02:08:07 +0800

    Change frequency of scheduled jobs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,4 +6,4 @@ export const api = functions.region('asia-east2').https.onRequest((req, res) => 
   res.set({ 'Access-Control-Allow-Origin': '*' });
   express(req, res);
 });
-export const cacheCourseListSnapshot = functions.region('asia-east2').pubsub.schedule('every day at 05:00').timeZone('Asia/Singapore').onRun(scheduled.cacheCourseListSnapshot);
+export const cacheCourseListSnapshot = functions.region('asia-east2').pubsub.schedule('every monday 05:00').timeZone('Asia/Singapore').onRun(scheduled.cacheCourseListSnapshot);
