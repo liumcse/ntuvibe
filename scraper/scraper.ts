2dfe92e94f75009af35d3aaa1ad00114a250fff9
Ming <liumcse@gmail.com>
2020-09-20 22:33:16 +0800

    Amend

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -30,7 +30,9 @@ function execCommandAsync(command: string) {
 /** Initialize by installing CLI. */
 async function initialize() {
   // Install dependencies
-  await execCommandAsync(`cd ${CLI_DIRECTORY} && python3 setup.py install`);
+  await execCommandAsync(
+    `cd ${CLI_DIRECTORY} && apt-get install python3-setuptools && python3 setup.py install`
+  );
   console.log("CLI installed.");
   // Create folder
   if (!fs.existsSync(path.join(__dirname, "./output"))) {
